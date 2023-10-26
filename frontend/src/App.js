8e36e1baf4efd7b72d8af3114b1491645a921d88
Israel Bonilla <israel.bonillad@gmail.com>
2023-10-25 22:12:54 -0500

    check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { AboutUs } from './pages/AboutUs';
 import { Contact } from './pages/Contact';
+import Register from './pages/register';
 import './styles/App.css';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/#" element={<Home />} />  {/* Ruta para la página Register */}
+        <Route path="/register" element={<Register />} />  {/* Ruta para la página Register */}
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/register.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/register.css';
+
+function Register() {
+    return (
+        <div className='bodyregister'>
+        <div className="register-container">
+            <div className="register-header">
+                <h2>Bienvenido a KanisTech</h2>
+                <p>Registro</p>
+            </div>
+            <form className="register-form">
+                <input type="text" placeholder="Nombre completo" required />
+                <input type="email" placeholder="Correo electrónico" required />
+                <input type="password" placeholder="Contraseña" required />
+                <input type="password" placeholder="Confirmar contraseña" required />
+                <button type="submit">Registrarse</button>
+            </form>
+        </div>
+        </div>
+    );
+}
+
+export default Register;
