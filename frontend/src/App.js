bec78f47cf77e6ba48c13042cc15abc438c514b6
Israel Bonilla <sebastian.bonillad@gmail.com>
2023-10-25 21:48:16 -0500

    arreglado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { AboutUs } from './pages/AboutUs';
 import { Contact } from './pages/Contact';
-import Register from './pages/register';  // Importación de la página Register
 import './styles/App.css';
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/register" element={<Register />} />  {/* Ruta para la página Register */}
+        <Route path="/#" element={<Home />} />  {/* Ruta para la página Register */}
       </Routes>
     </Router>
   );
