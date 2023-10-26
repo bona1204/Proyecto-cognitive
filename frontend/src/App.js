4d69d0fee5e876d357326ca492b09ba513fc3881
Israel Bonilla <israel.bonillad@gmail.com>
2023-10-25 20:37:48 -0500

    registerhtml

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {Home} from './pages/Home';
-import {AboutUs} from './pages/AboutUs';
-import {Contact} from './pages/Contact';
+import { Home } from './pages/Home';
+import { AboutUs } from './pages/AboutUs';
+import { Contact } from './pages/Contact';
+import Register from './pages/register';  // Importación de la página Register
 import './styles/App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<Home/>} />
-        <Route path="/about" element={<AboutUs/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />  {/* Ruta para la página Register */}
       </Routes>
     </Router>
   );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';  // Importa Link desde react-router-dom
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/Home.css';
@@ -11,9 +12,9 @@ export function Home() {
         <div className="main-container">
           <div className="content">
             <h1>Bienvenido a KanisTech</h1>
-            <h2>El mejor servicio de riego tecnificado</h2>
-            <p>Riega de manera automática...</p>
-            <a href="#" className="button">Regístrate</a>
+            <h2>Innovación en Riego Automatizado</h2>
+            <p>Optimiza tu cultivo con tecnología avanzada y sostenible...</p>
+            <Link to="/register" className="button">Regístrate</Link>  {/* Modifica el <a> a un componente <Link> para dirigir a la página Register */}
           </div>
         </div>
       </div>
