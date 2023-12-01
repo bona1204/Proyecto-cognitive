44e484e45620342987514c68bdccb930c9f1862b
rosquiya <rosario.quispe@utec.edu.pe>
2023-12-01 16:49:02 -0500

    cambios0112v3

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import userIcon from '../assets/images/user.png';
-// Importa otras imágenes de manera similar
-
 import '../styles/dashboard.css';
 
 function Dashboard() {
+  // Manejador de clic para cerrar sesión
+  const handleLogout = () => {
+    // Lógica para cerrar sesión (por ejemplo, eliminar tokens, redireccionar, etc.)
+    console.log('Cerrar Sesión');
+
+    // Simulación de redirección a la página de inicio de sesión
+    window.location.href = '/login'; // Reemplaza con tu lógica real de redirección
+  };
+
   return (
     <div className="bodyDashboard">
       <div className="dashboard">
@@ -15,17 +22,20 @@ function Dashboard() {
           <div className="index1">
             <div className="fondo-index"></div>
             <b className="kanistech">KANISTECH</b>
-            
+
             <div className="user-container">
-                <b className="user">ROSARIO QUISPE</b>
+              <b className="user">Usuario</b>
+              <a href="#" className="logout-link" onClick={handleLogout}>
+                Cerrar Sesión
+              </a>
             </div>
-           </div>
+          </div>
 
           <div className="cabecera">
             <div className="fondo-cabecera"></div>
             <b className="auto-water">AUTO WATER </b>
           </div>
-          {/* aqui embeber el power bi*/}
+          {/* aquí embeber el power bi*/}
           <img className="powerbi-icon" alt="" src={require('../assets/images/user.png').default} />
         </div>
       </div>
